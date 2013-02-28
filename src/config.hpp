41a43e7de43c34dd0f083a6ca88f200c1549d2d3
Bartek Szurgot <bartek.szurgot@baszerr.eu>
2013-02-28 18:12:37 +0100

    RC frequency is 9.6/8==1.2MHz

--- src/config.hpp
+++ src/config.hpp
@@ -11,7 +11,7 @@
 constexpr uint32_t KHz=1000L;
 constexpr uint32_t MHz=1000L*KHz;
 
-// quarc frequency in [Hz] (needs to be a define...)
-#define F_CPU (1*MHz)
+// clock frequency in [Hz] (needs to be a define...)
+#define F_CPU (1*MHz+200*KHz)
 
 #endif
