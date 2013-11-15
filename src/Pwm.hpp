bf7e85044745e0f4e0efa85ad9eb3c40274681f6
bartek 'basz' szurgot <git@baszerr.eu>
2013-11-15 17:02:10 +0100

    fixed values being set

--- src/Pwm.hpp
+++ src/Pwm.hpp
@@ -15,26 +15,26 @@ public:
     static_assert(Pin::ctrlLed == _BV(PB0), "PWM0 set to unknown channel");
     static_assert(Pin::light   == _BV(PB1), "PWM1 set to unknown channel");
     // set waveform generation mode
-    TCCR0A |=  WGM01|WGM00;     // set fast pwm mode
-    TCCR0B &= ~(FOC0A|FOC0B);   // required by the spec, though match with default values
+    TCCR0A |=  _BV(WGM01)|_BV(WGM00);   // set fast pwm mode
+    TCCR0B &= ~(_BV(FOC0A)|_BV(FOC0B)); // required by the spec, though match with default values
     // set prescaler to N; (9.6*10^6/8)/(N*256)
-    TCCR0B |= CS01|CS01;        // N=8 => 585.9375[Hz] ~= 600[Hz]
-    // set OC0x work mode
+    TCCR0B |= _BV(CS01);                // N=8 => 585.9375[Hz] ~= 600[Hz]
+    // set OC0x work modes
     enableA(false);
     enableB(false);
     // enable interrupts from T0
-    TIMSK0 |= TOIE0;            // enable T0 interrupts
+    TIMSK0 |= _BV(TOIE0);               // enable T0 interrupts
   }
 
   void enableA(const bool e)
   {
     // clear OC0A on match, set on TOP.
-    enableImpl(e, COM0A1|COM0A0, COM0A1);
+    enableImpl(e, _BV(COM0A1)|_BV(COM0A0), _BV(COM0A1));
   }
   void enableB(const bool e)
   {
     // clear OC0B on match, set on TOP.
-    enableImpl(e, COM0B1|COM0B0, COM0B1);
+    enableImpl(e, _BV(COM0B1)|_BV(COM0B0), _BV(COM0B1));
   }
 
   void fillA(const uint8_t fill) { OCR0A = fill; }
